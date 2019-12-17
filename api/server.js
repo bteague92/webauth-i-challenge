35fd96a9825cbf75cd929a508d28e1e591adcff9
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-12-17 17:17:13 -0500

    finished day 2 mvp

--- api/server.js
+++ api/server.js
@@ -1,12 +1,45 @@
-const express = require('express');
-
-const apiRouter = require('./api-router.js');
-const configureMiddleware = require('./configure-middleware.js');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const sessions = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(sessions);
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const knex = require("../database/dbConfig.js");
 
 const server = express();
 
-configureMiddleware(server);
+const sessionConfiguration = {
+    name: "chocolatechip",
+    secret: "keep it secret, keep it safe!",
+    saveUninitialized: true,
+    resave: false,
 
-server.use('/api', apiRouter);
+    store: new KnexSessionStore({
+        knex,
+        createtable: true,
+
+        clearInterval: 1000 * 60 * 10,
+        sidfieldname: "sid",
+        tablename: "sessions",
+    }),
+
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false,
+        httpOnly: true,
+    },
+};
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(sessions(sessionConfiguration));
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+server.get('/', (req, res) => {
+    res.json({ message: "its working!!!!!" });
+});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,10 @@
 const bcrypt = require("bcryptjs");
-
 const router = require("express").Router();
-
 const Users = require("../users/users-model.js");
 
 router.post("/register", (req, res) => {
   let user = req.body;
-
   const hashedPassword = bcrypt.hashSync(user.password, 8);
-
   user.password = hashedPassword;
 
   Users.add(user)
@@ -27,7 +23,8 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        req.session.user = user;
+        res.status(200).json({ message: `Welcome back ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
@@ -35,6 +32,23 @@ router.post("/login", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({
+          message:
+            "logging out didnt work",
+        });
+      } else {
+        res.status(200).json({ message: "logged out" });
+      }
+    });
+  } else {
+    res.status(200).end();
+  }
 });
 
 module.exports = router;
